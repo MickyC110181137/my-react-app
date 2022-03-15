58fe48bba8cf4940fce42127fc0c5658d5aa7980
MickyC110181137 <93572273+MickyC110181137@users.noreply.github.com>
2022-03-15 22:06:04 +0800

    react

--- src/App.js
+++ src/App.js
@@ -6,13 +6,23 @@ function App() {
   const [number, setNumber] =useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <div>{number}</div>
-        <button onClick={()=>{setNumber(number+1)}}>加一</button>
-        <button onClick={()=>{setNumber(number-1)}}>減一</button>
-        <div>yeah</div>
-      </header>
-      
+      <main>
+        <div className="container">
+          <h1>CCCounter</h1>
+          <div className='size'>
+            <span className= {`value ${number > 0 ? 'text-green': 'text-red'} ${number === 0 && 'text-black'}` }>
+              {number}
+            </span>
+          </div>
+  
+          <div className="button-container">
+            <button className="btn decrease" onClick= {() =>{setNumber(number - 1);}}>decrease</button>
+            <button className="btn reset" onClick= {() =>{setNumber(0);}}>reset</button>
+            <button className="btn increase" onClick= {() =>{setNumber(number + 1);}}>increase</button>
+          </div>
+        </div>
+      </main>
+
     </div>
   );
 }
