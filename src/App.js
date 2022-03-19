f982ccda6ee402e047de29effd6a46d16da8dad1
MickyC110181137 <93572273+MickyC110181137@users.noreply.github.com>
2022-03-20 02:12:40 +0800

    img

--- src/App.js
+++ src/App.js
@@ -20,24 +20,24 @@ function App() {
               <label for="word" class="form__label">パスワードを取ります</label>
             </div>
           </div>
-          
+          <div className='img'></div>
           <div className="container">
-          <h1>CCCounter</h1>
-          <div className='size'>
-            <span className= {`value ${number > 0 ? 'text-green': 'text-red'} ${number === 0 && 'text-black'}` }>
-              {number}
-            </span>
-          </div>
+            <h1>CCCounter</h1>
+            <div className='size'>
+              <span className= {`value ${number > 0 ? 'text-green': 'text-red'} ${number === 0 && 'text-black'}` }>
+                {number}
+              </span>
+            </div>
   
-          <div className="button-container">
-            {number > -5 &&<button className="btn decrease" onClick= {() =>{setNumber(number - 1);}}>decrease</button>} 
-            <button className="btn reset" onClick= {() =>{setNumber(0);}}>reset</button>
-            {number < 5 &&<button className="btn increase" onClick= {() =>{setNumber(number + 1);}}>increase</button>}
+            <div className="button-container">
+              {number > -5 &&<button className="btn decrease" onClick= {() =>{setNumber(number - 1);}}>decrease</button>} 
+              <button className="btn reset" onClick= {() =>{setNumber(0);}}>reset</button>
+              {number < 5 &&<button className="btn increase" onClick= {() =>{setNumber(number + 1);}}>increase</button>}
+            </div>
           </div>
+          
         </div>
 
-        </div>
-        
       </main>
 
     </div>
