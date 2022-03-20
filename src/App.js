c8308d3ba498eaf6b0cdb2857c33473fc0862862
MickyC110181137 <93572273+MickyC110181137@users.noreply.github.com>
2022-03-20 16:49:40 +0800

    picture

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import logo from './logo.svg';
-import './App.css';
+/*import './App.css';
 import {useState} from "react"
 
 function App() {
   const [number, setNumber] =useState(0)
+  const slider_span1 = props  => {
+    <div id = {props.id}> something </div>
+   }
   return (
     <div className="App">
       <main>
+
         <div className='row'>
           <div className='Login'>
-            <div class='form'>
-              <input type="text" id="email" class="form__input" autoComplete="off" placeholder=" "/>
-              <label for="email" class="form__label">你の帳號</label>
+            <div className='box'>
+              <h1>LOGIN</h1>
             </div>
-            <br/>
-            <br/>
-            <div class='form'>
-              <input type="password" id="word" class="form__input" autoComplete="off" placeholder=" "/>
-              <label for="word" class="form__label">汝密碼，吾收之</label>
+            <div className='box'>
+              <div class='form'>
+                <input type="text" id="email" class="form__input" autoComplete="off" placeholder=" "/>
+                <label for="email" class="form__label">你の帳號</label>
+              </div>
+            </div>
+            <div className='box'>
+              <div class='form'>
+                <input type="password" id="word" class="form__input" autoComplete="off" placeholder=" "/>
+                <label for="word" class="form__label">汝密碼，吾收之</label>
+              </div>
+            </div>
+            <div className='box'>
+              <div className='boxer' method="post">
+                <input type="submit" name="" value="Login"></input>
+              </div>
             </div>
           </div>
-          <div className='img'></div>
           <div className="container">
-            <h1>CCCounter</h1>
+            <h1>COUNTER</h1>
             <div className='size'>
               <span className= {`value ${number > 0 ? 'text-green': 'text-red'} ${number === 0 && 'text-black'}` }>
                 {number}
@@ -35,13 +48,41 @@ function App() {
               {number < 5 &&<button className="btn increase" onClick= {() =>{setNumber(number + 1);}}>increase</button>}
             </div>
           </div>
-          
-        </div>
+          <div className='flex'>
+              <span class="slider-span" id="slider-span1"></span>
+              <span class="slider-span" id="slider-span2"></span>
 
+              <div class="img-slider">
+                <div class="slides-div" id="slide-1">
+                  <img src={require('kick00.png')} alt="" class="img1" id="img1"></img>
+                  <button class="button1" id="button-1"></button>
+                </div>
+              </div>
+              <div class="img-slider">
+                <div class="slides-div" id="slide-2">
+                  <img src={require('runback00.png')} alt="" class="img2" id="img2"></img>
+                    <button class="button2" id="button-2"></button>
+                </div>
+            </div>
+            
+          </div>
+        
+        </div>
       </main>
 
     </div>
   );
+}
+*/
+import './App.css';
+import Slider from'./Components/Slider/Slider.js';
+
+function App() {
+return(
+<>
+<Slider/>
+</>
+);
 }
 
 export default App;
