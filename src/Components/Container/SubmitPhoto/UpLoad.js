98e168a065b8fed94a9171c00c4c55ac44be223d
MickyC110181137 <93572273+MickyC110181137@users.noreply.github.com>
2022-04-23 00:23:03 +0800

    api

--- src/Components/Container/SubmitPhoto/UpLoad.js
+++ src/Components/Container/SubmitPhoto/UpLoad.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Png from "./1x.png";
 import axios from "axios";
 // import { callUploadPhoto, callGetAlbum } from "../../../api/api";
 
 export default function Upload() {
   const [imgUrl, setImgUrl] = useState("");
+  const [album, setAlbum] = useState([]);
+
+  useEffect(() => {
+    const getImage = async () => {
+      const image3 = await axios({
+        method: "GET",
+        url: "https://api.imgur.com/3/album/KMmOgBr",
+        headers: {
+          Authorization: "Bearer " + process.env.REACT_APP_NOT_SECRET_CODE,
+        },
+      });
+      setAlbum(image3.data.data.images);
+    };
+    getImage();
+  }, [imgUrl]);
+
   console.log(
     " process.env.REACT_APP_NOT_SECRET_CODE>>",
     process.env.REACT_APP_NOT_SECRET_CODE
@@ -14,7 +30,7 @@ export default function Upload() {
     const image = event.target.files[0];
     data.append("image", image);
 
-    data.append("aldum", "KMmOgBr");
+    data.append("album", "KMmOgBr");
     data.append("title", image);
     console.log("Bearer " + process.env.REACT_APP_NOT_SECRET_CODE);
 
@@ -32,7 +48,19 @@ export default function Upload() {
   return (
     <main>
       <form id="form1" runat="server">
-        <div className="row">
+        <div className="gridalbum">
+          <div className="flex">
+            {album.map((album1) => {
+              return (
+                <img
+                  key={album1.id}
+                  src={album1.link}
+                  width={100}
+                  height={100}
+                ></img>
+              );
+            })}
+          </div>
           <input
             id="uploadfile"
             type={"file"}
@@ -40,7 +68,7 @@ export default function Upload() {
             onChange={onFileUpload}
           />
           <label htmlFor="uploadfile" type="button1">
-            <img src={imgUrl || Png}></img>
+            <img src={imgUrl || Png} width={60}></img>
           </label>
         </div>
       </form>
