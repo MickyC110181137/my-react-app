0899b0576e4c0042ad1638f4e73b8bf6797d76d9
MickyC110181137 <93572273+MickyC110181137@users.noreply.github.com>
2022-10-23 21:54:15 +0800

    Overview,ref

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import Cursor from "./Components/cursor/cursor";
 import Menu from "./Components/Container/Menu/Menu";
 import Intro from "./Components/Container/Intro/Intro";
+import Overview from "./Components/Container/Overview/Overview";
 
 function App() {
   const [toggle, setToggle] = useState(false);
+  const Chcolor = useRef();
 
   useEffect(() => {
-    const Chcolor = document.getElementById("Chcolor");
     window.addEventListener("scroll", () => {
       if (window.scrollY > 650 && window.scrollY < 1160) {
-        Chcolor.style.backgroundColor = `rgba(${(1160 - window.scrollY) / 2}, ${
+        Chcolor.current.style.backgroundColor = `rgba(${
           (1160 - window.scrollY) / 2
-        }, ${(1160 - window.scrollY) / 2})`;
+        }, ${(1160 - window.scrollY) / 2}, ${(1160 - window.scrollY) / 2})`;
       }
     });
   }, []);
@@ -26,7 +27,11 @@ function App() {
           <Menu toggle={toggle} />
         </div>
 
-        <div className="body" id="Chcolor" style={{ backgroundColor: "white" }}>
+        <div
+          ref={Chcolor}
+          className="body"
+          style={{ backgroundColor: "white" }}
+        >
           <Intro />
           <Cursor />
           <div
@@ -36,7 +41,7 @@ function App() {
               width: "100%",
             }}
           >
-            12313
+            <Overview />
           </div>
         </div>
 
---
+++ src/Components/Container/Overview/Overview.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Overview.css";
+
+export default function Overview() {
+  return (
+    <div className="Profile">
+      <div className="Profile2">
+        <div className="textbox">sadfsad</div>
+        <div className="textAni"></div>
+      </div>
+      <div className="Profile2">majsudklo</div>
+    </div>
+  );
+}
