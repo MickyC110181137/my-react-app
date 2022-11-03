ebf93ef729616c2e777862e11150394222528768
MickyC110181137 <93572273+MickyC110181137@users.noreply.github.com>
2022-11-03 19:16:21 +0800

    contact footer nav

--- src/Components/Principal/Contact/Contact.js
+++ src/Components/Principal/Contact/Contact.js
@@ -1,7 +1,75 @@
 import React from "react";
 import "./Contact.css";
+import { IoMdMail } from "react-icons/io";
+import { SiMessenger, SiWhatsapp } from "react-icons/si";
+
 const Contact = () => {
-  return <section id="contact">contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In touch</h5>
+      <h2>Contact</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <IoMdMail className="contact_option-icon" />
+            <h4>Email</h4>
+            <h5>dummyegatori@gmail.com</h5>
+            <a
+              href="mailto:1209436216micky@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact_option">
+            <SiMessenger className="contact_option-icon" />
+            <h4>Messenger</h4>
+            <h5>dummyegatori@gmail.com</h5>
+            <a
+              href="https://m.me/profile.php?id=100013102720729"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact_option">
+            <SiWhatsapp className="contact_option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>dummyegatori@gmail.com</h5>
+            <a
+              href="https://api.whatsapp.com/send?"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/Components/Principal/Footer/Footer.js
+++ src/Components/Principal/Footer/Footer.js
@@ -1,8 +1,60 @@
 import React from "react";
 import "./Footer.css";
 
+import {
+  AiOutlineFacebook,
+  AiOutlineInstagram,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        EGATOR
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://facebook.com">
+          <AiOutlineFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <AiOutlineInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <AiFillTwitterCircle />
+        </a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; EGATOR Tutorials. All rights reservice</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
